dfd54c3b0bdbfed6a8d74b2a339eb2cdc2ca5e11
David Russell <davidr0013@gmail.com>
2018-01-13 13:30:35 -0800

    Create web development git folder

---
+++ Lab1/Lab1/Lab1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Person
+    {
+        string name;
+        int age;
+        string style;
+    }
+}
+
