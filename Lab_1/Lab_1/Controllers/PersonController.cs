ed77de09c8a70dc7a05e881cf3c8739902c953b2
David Russell <davidr0013@gmail.com>
2018-01-19 17:21:46 -0800

    Added lab work

---
+++ Lab_1/Lab_1/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Garry";
+            ViewBag.Age = 57;
+            ViewBag.Shoes = "Slippers";
+            return View();
+        }
+    }
+}
---
+++ Lab_1/Lab_1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_1.Models
+{
+    public class Person
+    {
+        string Name;
+        int Age;
+        string Shoes;
+    }
+}
